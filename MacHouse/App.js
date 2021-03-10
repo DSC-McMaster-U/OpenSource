f00fe50c7dc043b1abd14f36a29a846a91c6bdbf
HarrisonOssias <ossiash@mcmaster.ca>
2021-03-10 13:48:05 -0500

    first sample test

--- MacHouse/App.js
+++ MacHouse/App.js
@@ -1,30 +1,30 @@
 import React from 'react';
-import { StatusBar } from "expo-status-bar";
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, SafeAreaView, StatusBar } from 'react-native';
-import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import Calendar from './screens/Calendar';
-import ChatScreen from "./screens/chatScreen";
-import HomeScreen from "./screens/HomeScreen";
+import ChatScreen from './screens/chatScreen';
+import HomeScreen from './screens/HomeScreen';
 import Cards from './screens/dashboard';
 
 const theme = {
-  ...DefaultTheme,
-  roundness: 2,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: "#7b8be0",
-    accent: "#850044",
-  },
+	...DefaultTheme,
+	roundness: 2,
+	colors: {
+		...DefaultTheme.colors,
+		primary: '#7b8be0',
+		accent: '#850044',
+	},
 };
 
 export default function App() {
 	return <Calendar></Calendar>;
-  return <ChatScreen />;
-  <PaperProvider theme={theme}>
-      <Management />
-  </PaperProvider>
-  return <HomeScreen />;
-  return <Cards />;
+	return <ChatScreen />;
+	<PaperProvider theme={theme}>
+		<Management />
+	</PaperProvider>;
+	return <HomeScreen />;
+	return <Cards />;
 }
 
 const styles = StyleSheet.create({
@@ -33,4 +33,3 @@ const styles = StyleSheet.create({
 	},
 });
 export default App;
-
